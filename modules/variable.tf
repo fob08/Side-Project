f63eee82c6877cfa6e7c3afc9864478c5f0d60b8
bytewavetechnologies <info@bytewave-technologies.com>
2024-04-20 16:09:00 +0200

    image registry

--- modules/variable.tf
+++ modules/variable.tf
@@ -28,3 +28,57 @@ variable "lb_port" {
   type = number
   default = 80
 }
+
+# s3 bucket name
+variable "bucket_prefix" {
+  type = string
+  description = "The prefix to be used for s3 bucket creation"
+  default = "s3-bucket"
+}
+
+variable "region" {
+  type = string
+  description = "AWS region name to create and manage resources in"
+#  default = "eu-central-1"
+}
+
+variable "force_delete" {
+  description = "If `true`, will delete the repository even if it contains images. Defaults to `false`"
+  type        = bool
+  default     = false
+}
+variable "ecr" {
+  type = string
+  description = "This is the name of the elastic container registry"
+
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository. Must be one of: `MUTABLE` or `IMMUTABLE`."
+  type        = string
+  default     = "MUTABLE"
+  validation {
+    condition     = contains(["MUTABLE", "IMMUTABLE"], var.image_tag_mutability)
+    error_message = "The image_tag_mutability must be either MUTABLE or IMMUTABLE."
+  }
+}
+
+variable "scan_on_push" {
+  description = "Indicates whether images are scanned after being pushed to the repository (true) or not scanned (false)."
+  type        = bool
+  default     = true
+}
+
+# Policy
+variable "policy" {
+  description = "Manages the ECR repository policy"
+  type        = string
+  default     = null
+}
+
+# Lifecycle policy
+variable "lifecycle_policy" {
+  description = "Manages the ECR repository lifecycle policy"
+  type        = string
+  default     = null
+}
